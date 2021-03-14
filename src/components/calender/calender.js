c45a0211a810992c05b8be861b765f9c5e85cbfd
ForgottenTale <abhijithkannan452@gmail.com>
2021-03-14 23:41:32 +0530

    update

--- src/components/calender/calender.js
+++ src/components/calender/calender.js
@@ -7,80 +7,92 @@ function Calender() {
 
 
     const [days, setDays] = useState([]);
-
-    const [today,setToday] = useState([]);
+    const [today, setToday] = useState([]);
+    const [next, setNext] = useState(0);
     var dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    
 
-    
-    useEffect(()=>{
-        var monthNames = ["Januvary","February","March","April","May","June","July","August","September","October","Novemeber","December"];
+
+
+    useEffect(() => {
+        var monthNames = ["Januvary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
         let d = new Date();
         let day = d.getDate();
         // d.setMonth(3)
         // d.setFullYear(2020)
+        if (next > 0) {
+            d.setMonth(d.getMonth()+1);
+        }
+        else if (next < 0) {
+            d.setMonth(d.getMonth()-1);
+        }
         let month = d.getMonth();
         let year = d.getFullYear();
-        setToday({"day":day,"month":monthNames[month],"year":year});
-        var daysInMonth = new Date(year,month+1,0).getDate();
+        console.log(month);
+       
+        setToday({ "day": day, "month": monthNames[month], "year": year });
+        var daysInMonth = new Date(year, month + 1, 0).getDate();
         d.setDate(1);
         let firstDayIndex = d.getDay();
-        let lastDayIndex = new Date(d.getFullYear(),d.getMonth() + 1,0).getDay();
-        var prevMonthLastDay = new Date(d.getFullYear(),d.getMonth(),0).getDate();
-        
+        let lastDayIndex = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDay();
+        var prevMonthLastDay = new Date(d.getFullYear(), d.getMonth(), 0).getDate();
+
         var temp = [];
         var a = new Date();
         var p = [];
-        for(let i=firstDayIndex; i>0;i--){
+        for (let i = firstDayIndex; i > 0; i--) {
             a.setDate(prevMonthLastDay);
-            a.setMonth(month-1);
-            p.push({"day": prevMonthLastDay,"key":a.toLocaleDateString() })
-            prevMonthLastDay =  prevMonthLastDay - 1;
+            a.setMonth(month - 1);
+            p.push({ "day": prevMonthLastDay, "key": a.toLocaleDateString() })
+            prevMonthLastDay = prevMonthLastDay - 1;
         }
-        for(let i=firstDayIndex-1; i>=0;i--){
+        for (let i = firstDayIndex - 1; i >= 0; i--) {
             temp.push(p[i]);
         }
-        console.log(daysInMonth)
-        for(let i = 1; i<=daysInMonth;i++){
+   
+        for (let i = 1; i <= daysInMonth; i++) {
             a.setDate(i);
             a.setMonth(month);
-            temp.push({"day": i ,"key":a.toLocaleDateString() })
+            temp.push({ "day": i, "key": a.toLocaleDateString() })
         }
-        
-        for(let i=lastDayIndex; i<6;i++){
-            a.setDate(i- lastDayIndex +1);
-            a.setMonth(month+1);
-            temp.push({"day": i- lastDayIndex +1,"key":a.toLocaleDateString() })
-            
+
+        for (let i = lastDayIndex; i < 6; i++) {
+            a.setDate(i - lastDayIndex + 1);
+            a.setMonth(month + 1);
+            temp.push({ "day": i - lastDayIndex + 1, "key": a.toLocaleDateString() })
+
         }
         setDays(temp)
-    },[])
+    }, [next])
 
 
-    const nextMonth =(props)=>{
-        console.log(props.day)
+    const nextMonth = () => {
+       
+        setDays([]);
+        setNext(next + 1);
 
     }
-    const prevMonth =()=>{
-        console.log("Hi")
+    const prevMonth = () => {
+    
+        setDays([]);
+        setNext(next - 1);
     }
 
     function DateBody(props) {
-    
+
         if (props.day.day === new Date().getDate()) {
             return (
-                <div className="date" onClick={()=>console.log(props.day.key)}>
+                <div className="date" onClick={() => console.log(props.day.key)}>
                     <div className="date_day active" >{props.day.day}</div>
-                    {props.day.key}
+                    <p style={{color:"white"}}>{props.day.key}</p>
                 </div>
             );
         }
 
         else {
             return (
-                <div className="date" onClick={()=>console.log(props.day.key)} >
+                <div className="date" onClick={() => console.log(props.day.key)} >
                     <div className="date_day">{props.day.day}</div>
-                    {props.day.key}
+                    <p style={{color:"white"}}>{props.day.key}</p>
                 </div>
             );
         }
@@ -101,15 +113,15 @@ function Calender() {
         <div>
             <h2 className="today">{today.day} {today.month} {today.year}</h2>
             <div className="monthView">
-                
+
                 {dayName.map((name) => <DayNameBody day={name} key={name} />)}
-               
-                {(days===[])?{}:days.map((date) => <DateBody day={date} key={date.key}/>)}
-             
+
+                {(days === []) ? {} : days.map((date) => <DateBody day={date} key={date.key} />)}
+
             </div>
             <button onClick={prevMonth}>Prevous</button>
             <button onClick={nextMonth}>Next</button>
-            
+
         </div>
     );
 
