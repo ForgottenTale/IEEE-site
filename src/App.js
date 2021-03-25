395a61685934bd7a579cb33d2d57f46498720075
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-03-25 22:16:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Calender from './components/calender/calender';
+import Login from './components/login/login';
 
 function App() {
   return (
     <div className="App">
-      <Calender/>
-sasadasdasdas
+    <Login/>
     
     </div>
   );
