f8010eae9bfa4de6e6044ed72f340c1404650202
AleenaAnnaRobin <aleenaannarobin@gmail.com>
2021-03-28 18:58:58 +0530

    login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,10 +1,25 @@
 import './login.css'
 
-function Login(){
+export default function Login(){
 
     return(
-        <div className="login">
-            haddsf
+<div className="login">
+<h3>Login</h3>
+        <div class="user">
+            <label for="username">USERNAME</label>
+            <input type="text" name="username" id="user" placeholder="Username"/><br></br>
+            
         </div>
-    );
-}
+        <div class="pass">
+            <label for="password"> PASSWORD</label>
+            <input type="password" name="password" placeholder="PASSWORD" id="pass"/><br></br>
+        
+        </div>
+        <div>
+            <input class="button" type="submit" name="Login" value="Login" id="user"/>
+        </div>
+        
+</div>
+    )
+    
+    }
