9636d6ad1c0fc3520ec380f6009fa6e6e04ee786
AleenaAnnaRobin <aleenaannarobin@gmail.com>
2021-03-29 23:36:34 +0530

    login2

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,22 +4,33 @@ export default function Login() {
 
     return (
         <div className="login">
-            <h3>Login</h3>
+            
+            <h2>Login</h2>
+            <b>Login to your account</b><br></br>
+            
             <div className="user">
-                <label for="username">USERNAME</label>
-                <input type="text" name="username" id="user" placeholder="Username" /><br></br>
-
+                <label for="username">Username</label><br></br>
+                <input type="text" name="" placeholder="Email or Phone number" size="30"/><br></br>
+                
             </div>
             <div className="pass">
-                <label for="password"> PASSWORD</label>
-                <input type="password" name="password" placeholder="PASSWORD" id="pass" /><br></br>
-
+                <label for="password"> Password</label>
+                <input type="password" name="password" placeholder="Password" id="pass" size="30" /><br></br>
+                 
+            </div>
+        
+            <div className="check">
+                    <input type="checkbox"></input>
+                    <label for="Rem">Remember me</label>
             </div>
             <div>
-                <input className="button" type="submit" name="Login" value="Login" id="user" />
+                <input className="button"  type="submit" name="Login" value="Sign In" id="user" size="0" />
             </div>
-
-        </div>
+            
+            </div>
+            
+        
     )
-
+    
 }
+   
