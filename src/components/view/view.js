5e23bffea9a15b274cd3692b7adf994a7535d46f
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-04-14 20:01:23 +0530

    adding request view

--- src/components/view/view.js
+++ src/components/view/view.js
@@ -1,29 +1,104 @@
 import './view.scss';
 import pic from '../../images/pic3.jpg'
 
+const user = (data) => [
+    <center>
+        <img className="view_container_img" src={pic} alt=''></img>
+        <p>{data.name}</p>
+    </center>,
+    <div className="view_container_item">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="feather feather-mail"
+            viewBox="0 0 24 24"
+        >
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+            <path d="M22 6L12 13 2 6"></path>
+        </svg>
+    &nbsp; &nbsp;
+    <p>{data.email}</p>
+    </div>,
+    <div className="view_container_item">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="feather feather-user"
+            viewBox="0 0 24 24"
+        >
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+        </svg>
+    &nbsp; &nbsp;
+    <p>{data.role}</p>
+    </div>
+
+]
+
+const admin = (data) => [
+    <div className="view_container_item">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="feather feather-users"
+            viewBox="0 0 24 24"
+        >
+            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+            <circle cx="9" cy="7" r="4"></circle>
+            <path d="M23 21v-2a4 4 0 00-3-3.87M16 3.13a4 4 0 010 7.75"></path>
+        </svg>
+                       &nbsp; &nbsp;
+                       <p>{data.team}</p>
+    </div>
+]
+
+const request = (data) => [
+    <div className="view_container_item">
+        <p>Name : &nbsp;</p>
+        <p>{data.name}</p>
+    </div>,
+    <div className="view_container_item">
+        <p>Email : &nbsp;</p>
+        <p>{data.email}</p>
+    </div>,
+    <div className="view_container_item">
+        <p>Service : &nbsp;</p>
+        <p>{data.service}</p>
+    </div>,
+     <div className="view_container_item">
+     <p>Type : &nbsp;</p>
+     <p>{data.type}</p>
+ </div>,
+
+]
+
 export default function View({ data, setView, type }) {
     return (
         <div className="view">
             <div className="view_overlay" onClick={() => setView(false)}>
             </div>
             <div className="view_container">
-                <center>
-                    <img className="view_container_img"  src={pic} alt=''></img>
-                </center>
-                <div className="view_container_item">
-                    <p>Name :&nbsp;</p> <p>{data.name}</p>
-                </div>
-                <div className="view_container_item">
-                    <p>Email :&nbsp;</p> <p>{data.email}</p>
-                </div>
-                <div className="view_container_item">
-                    <p>Role :&nbsp;</p> <p>{data.role}</p>
-                </div>
-
-                {type === 'admin' ?
-                    <div className="view_container_item">
-                        <p>Team :&nbsp;</p> <p>{data.team}</p>
-                    </div> : null}
+                {type === "user" || type === "admin" ? user(data) : null}
+                {type === 'admin' ? admin(data) : null}
+                {type === 'request' ? request(data) : null}
             </div>
         </div>
     );
