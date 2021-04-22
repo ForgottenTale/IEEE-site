34f37e09492e7c594e7ee03ae002f8078b25fcc3
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-04-22 10:12:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 
 function App() {
   const [user, setUser] = useState(null);
-  return (user!== null ? <Router>
+  return (user=== null ? <Router>
     <div className="App">
         <Content />
       </div>
